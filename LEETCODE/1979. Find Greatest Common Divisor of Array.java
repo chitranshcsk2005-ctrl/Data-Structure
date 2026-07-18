07804891e71106353a338978d6e80abaf1bb2af6
chitransh srivastava <chitranshcsk2005@gmail.com>
2026-07-18 09:26:34 +0530

    Create 1979. Find Greatest Common Divisor of Array.java

---
+++ LEETCODE/1979. Find Greatest Common Divisor of Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findGCD(int[] nums) {
+        int a,b;
+        Arrays.sort(nums);
+        a=nums[0];
+        b=nums[nums.length-1];
+        while(b!=0)
+        {
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+}
